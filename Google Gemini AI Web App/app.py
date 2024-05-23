e23c8b3db7cca8c032342214bfbbf944733d580f
Vampire175 <rupalidhakate72@gmail.com>
2024-05-23 18:36:48 +0530

    Update app.py

--- Google Gemini AI Web App/app.py
+++ Google Gemini AI Web App/app.py
@@ -33,7 +33,7 @@ def index():
     return render_template('index.html', response_text=response_text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0')
 
 
 
